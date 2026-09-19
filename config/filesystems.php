0bbd9a499a537889d5c471eb782e08d7cddec574
yDewolf <andrersng@gmail.com>
2026-09-19 09:54:30 -0300

    chore: setup minio

--- config/filesystems.php
+++ config/filesystems.php
@@ -29,6 +29,17 @@ return [
     */
 
     'disks' => [
+        'minio' => [
+            'driver'                  => 's3',
+            'key'                     => env('MINIO_ACCESS_KEY_ID'),
+            'secret'                  => env('MINIO_SECRET_ACCESS_KEY'),
+            'region'                  => env('MINIO_DEFAULT_REGION', 'us-east-1'),
+            'bucket'                  => env('MINIO_DEFAULT_BUCKET'),
+            'endpoint'                => env('MINIO_ENDPOINT', 'http://minio:9000'),
+            'url'                     => env('MINIO_PUBLIC_URL', 'http://localhost:9000'),
+            'use_path_style_endpoint' => env('MINIO_USE_PATH_STYLE_ENDPOINT', true),
+            'throw'                   => false,
+        ],
 
         'local' => [
             'driver' => 'local',
